dc267a07a6e2e6d6163e9b8b4468437bba6c3df4
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-09-19 19:27:09 -0700

    Added more pseudo code to app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,22 @@
 //establish variables
 
-//
+// initialize libraries and APIs
+
+//create ajax calls to retrieve information from search bar on-click
+
+//submit search request from the user to IGDB
+
+//display results from IGDB in a list form
+
+//when game is selected by the user:
+    //send game name to Twitch API and record official game name to firebase
+    
+    //display game information from IGDB
+    
+    //display video feed from Twitch
+    
+    //display game name on page with search count information from firebase
+    
+    //display "trending" graph and/or consumer rating graph
+
+    
