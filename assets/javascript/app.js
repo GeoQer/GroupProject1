4c7c7b60b3259e97f1ab93511eca9d04eadd3a47
KodyShaw <kodyshaw16@gmail.com>
2018-09-22 17:36:58 -0700

    igdb Update. API build

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,15 +8,20 @@ var igdbQueryURL = "https://api-endpoint.igdb.com"
 var twitchClientID = "vaio0m3xzniwve47sl16xucnwvluef"
 var twitchQueryURL = "https://api.twitch.tv/helix"
 var twitchGameID = "33214" // hard coded game ID var
-var gameName = "Hitman"
+var gameName = "Diablo"
 var igdbID;
+var igdbNameReturn;
+var igdbSummaryReturn;
+var igdbEsrbReturn;
+var igdbGameImages = [];
+var twitchGameID;
 
 // Document ready function
 $(document).ready(function () {
 
     $.ajax({
         type: 'GET',
-        url: proxyServerURL + igdbQueryURL + '/games/?search='+ gameName + '&fields=*',
+        url: proxyServerURL + igdbQueryURL + '/games/?search=' + gameName + '&filter[category][eq]=0&fields=*&limit=15',
         dataType: 'json',
         headers: {
             'user-key': igdbClientID,
@@ -24,10 +29,10 @@ $(document).ready(function () {
         }
     }).then(function (response) {
         console.log(response);
-        igdbID = response[0].id;
+        igdbID = response[0].id;// The 0 will be what in the array the user selects ( add event grabers)
         console.log(igdbID);
-        
-    }).then(function(){
+
+    }).then(function () {
         $.ajax({
             type: 'GET',
             url: proxyServerURL + igdbQueryURL + '/games/' + igdbID + '?search=&fields=*',
@@ -37,13 +42,52 @@ $(document).ready(function () {
                 'Accept': 'application/json'
             }
         }).then(function (response) {
-            console.log(igdbID);
-            console.log(response);
+            igdbGameImages = []; // clears images
+
+           
+            console.log(response[0]);
+            igdbNameReturn = response[0].name;
+            igdbSummaryReturn = response[0].summary;
+            switch (response[0].esrb.rating) {
+                case 1:
+                    igdbEsrbReturn = "RP"
+                    break;
+                case 2:
+                    igdbEsrbReturn = "EC"
+                    break;
+                case 3:
+                    igdbEsrbReturn = "E"
+                    break;
+                case 4:
+                    igdbEsrbReturn = "E10+"
+                    break;
+                case 5:
+                    igdbEsrbReturn = "T"
+                    break;
+                case 6:
+                    igdbEsrbReturn = "M"
+                    break;
+                case 7:
+                    igdbEsrbReturn = "AO"
+            }
+
+            igdbGameImages.push(response[0].cover.url); 
+            for (let i = 0; i < response[0].screenshots.length; i++) {
+                igdbGameImages.unshift(response[0].screenshots[i].url);
+            }
             
+
+            
+            console.log(igdbGameImages[3]);
+
+            console.log("ID Number: " + igdbID + "- Game Name: " + igdbNameReturn);
+            console.log("Game Summary: " + igdbSummaryReturn);
+            console.log("Rated ESRB: " + igdbEsrbReturn);
+
         });
     })
 
-    
+
 
     function twitchSearchFunction() { //funtion for twitch search put into a funtion for callback reasons
         // ajax call for twitch to give game ID
@@ -56,13 +100,13 @@ $(document).ready(function () {
             },
         }).then(function (response) {
             console.log(response); //response for details of game from twitch
-            
+
             // This ajax below has been tested and works. but since it requires previous ajax information it wornt work without hardcodeing
         }).then( //only AFTER that is finished and we have the ID of game 
             //Run ajax to grab stream data from twich useing ID of game
             $.ajax({
                 type: 'GET',
-                url: twitchQueryURL + '/streams?game_id=' + gameID, //URL + the ID of game
+                url: twitchQueryURL + '/streams?game_id=' + twitchGameID, //URL + the ID of game
                 dataType: 'json',
                 headers: {
                     'Client-ID': twitchClientID,
@@ -73,7 +117,7 @@ $(document).ready(function () {
         );
     }
 
-    twitchSearchFunction;
+    // twitchSearchFunction();
 
 })
 
@@ -89,13 +133,13 @@ $(document).ready(function () {
 
 //when game is selected by the user:
     //send game name to Twitch API and record official game name to firebase
-    
+
     //display game information from IGDB
-    
+
     //display video feed from Twitch
-    
+
     //display game name on page with search count information from firebase
-    
+
     //display "trending" graph and/or consumer rating graph
 
-    
+
